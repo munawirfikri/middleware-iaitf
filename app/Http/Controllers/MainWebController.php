ef8c673447fb4d060d46df14a40238dd2e8d50ae
Munawir Fikri <munawirfikri@gmail.com>
2023-11-13 14:36:40 +0700

    modify get post title

--- app/Http/Controllers/MainWebController.php
+++ app/Http/Controllers/MainWebController.php
@@ -7,7 +7,11 @@ use Illuminate\Support\Facades\DB;
 class MainWebController extends Controller {
 
     public function getPostTitle(){
-        $data = DB::connection("mysql2")->table("wpi9_posts")->select('post_title')->orderBy('post_date', 'desc')->get();
+        $data = DB::connection("mysql2")
+        ->table("wpi9_posts")
+        ->select('post_title')
+        ->where('post_type', '=', 'post')
+        ->orderBy('post_date', 'desc')->get();
 
         $response['error'] = false;
         $response['message'] = 'Success';
